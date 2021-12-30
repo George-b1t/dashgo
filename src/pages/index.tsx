6490b886f32f0d00bacf56f1e0395585a0e20f7f
George-b1t <george.soares509@gmail.com>
2021-12-30 10:11:48 -0300

    integrando-recursos-aos-forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,31 @@ import {
   Stack,
 } from '@chakra-ui/react';
 
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormShcema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormShcema)
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  };
+
   return (
     <Flex
       w='100vw'
@@ -22,17 +44,22 @@ export default function Home() {
         p='8'
         borderRadius={8}
         flexDir='column'
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing='4'>
           <Input
             name='email'
             type='email'
             label='E-mail'
+            error={formState.errors.email}
+            {...register('email')}
           />
           <Input
             name='password'
             type='password'
             label='Senha'
+            error={formState.errors.password}
+            {...register('password')}
           />
         </Stack>
         <Button
@@ -40,6 +67,7 @@ export default function Home() {
           mt='6'
           colorScheme='pink'
           size='lg'
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
