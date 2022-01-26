7af0a9bcf701c2c113afd5400ae018daf89d1c0a
George-b1t <george.soares509@gmail.com>
2022-01-26 15:44:55 -0300

    Preparando MirajeJS para Paginacao de Users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
   name: string;
@@ -11,6 +11,7 @@ export function makeServer() {
     models: {
       user: Model.extend<Partial<User>>({})
     },
+
     factories: {
       user: Factory.extend({
         name(i: number) {
@@ -31,14 +32,31 @@ export function makeServer() {
         }
       })
     },
+
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
+
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', (schema, request) => {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = schema.all('user').models.slice(pageStart, pageEnd);
+
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        );
+      });
       this.post('/users');
 
       this.namespace = "";
